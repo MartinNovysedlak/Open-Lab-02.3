a71060be81c9e590ab8f79c483a6f6060637e2ce
MartinNovysedlak <martinnovysedlak48@gmail.com>
2019-10-09 21:40:12 +0200

    Initial

---
+++ Open-Lab-02.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Open_Lab_02._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = a % b;
+            Console.WriteLine(c);
+        }
+    }
+}
